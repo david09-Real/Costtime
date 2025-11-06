832b8f333063e7e2e50a606bf05ba182ff2ac4d3
Julio Cesar Camargo Martins <juliocesardarnox@gmail.com>
2025-11-05 22:37:18 -0300

    estilos e fazer inputs
    ja fiz o background com gradient agora falta os inputs e outros tipos de coisa

--- Costtime/app/(tabs)/index.tsx
+++ Costtime/app/(tabs)/index.tsx
@@ -3,19 +3,32 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import './styles.css';
+import { red } from 'react-native-reanimated/lib/typescript/Colors';
+import React, { useState } from 'react';
 
-<LinearGradient
-  colors={['#4c669f', '#3b5998', '#192f6a']}
-  style={styles.linearGradient}
->
-  {/* Outros elementos do seu componente aqui */}
-</LinearGradient>
 
 
 export default function TabOneScreen() {
   return (
-    <view  style={styles.linearGradient}>
+    <view  style={styles.total}>
+<LinearGradient
+  colors={['#E65729','#000000', '#343434ff']}
+
+locations={[0, 0.3, 1]}
+
+style = {styles.linear}
+> 
+<Text style={styles.textotitulo}>
+Bem Vindo de volta!
+</Text>
+
+
+<Text style={styles.textonormal}>
+Preencha os seus dados ou continue com as redes sociais
+
+</Text>
+
+</LinearGradient>
 
 
 
@@ -30,8 +43,28 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
-linearGradient: (
+linear: {
 
-  
-),
+height: '100%',
+width: '100%',
+
+},
+total: {
+flex: 1
+
+},
+textotitulo:{
+fontSize: 25,
+fontFamily: 'Roboto',
+color: 'white',
+fontWeight: 'bold'
+
+
+},
+textonormal:{
+color: 'white',
+fontSize: 16,
+fontFamily: 'Roboto'
+
+}
 });
